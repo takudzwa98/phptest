10069b5f91f6cf122475f89198e74390cedeacee
Takudzwa Karigomba <takkarigomba@gmail.com>
2019-11-18 10:33:47 +0000

    working

---
+++ app.js
@@ -0,0 +1,35 @@
+ReactDom.reader(
+  React.createElement(
+    "h1",
+    {"style":{"color": "red"}},
+    "Hello World"
+
+  ),
+  document.getElementById("root")
+);
+// Event listener to watch for clicks on link in HTML
+//   .querySelector('#productsContainer>li>a')
+//   .addEventListener('click', fetchProducts);
+
+document.addEventListener('DOMContentLoaded', fetchProducts)
+
+// Function runs when link in HTML is clicked.
+// Uses fetch to get JSON content from products.php
+// (which in turn retrieves them from DB)
+function fetchProducts() {
+
+  fetch('db-connect.php')
+    .then(res  => res.json())
+    .then(json => {
+
+      // Replace with updating react template bindings
+      // Here: just changes the innerHTML of ul#productsContainer
+      document
+        .querySelector('#productsContainer')
+        .innerHTML = json.map(product => `
+          <li>${product.name}: ${product.price}</li>
+        `).join('')
+
+    });
+}
+
